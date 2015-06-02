cbf73588d4dbaa12ed310cddf874ed16d8fba3bf
unknown <b7godin@SCHB7MW7321.schb7.swisslog.net>
2015-06-02 09:38:03 +0200

    Continue with testing of bugfixes done to advanced graphic image rendering. Workin against primefaces 3.5 and branch 1.0.1-SNAPSHOT. In this case, determine that the concept of closing the input stream done by GraphicImageManager.registerImage is not a good idea. Primefaces itself does not close the input streams. These input streams normally do not correspond to file pointer they are in memory buffers of an image uploaded that the application will eventually save to a file, for example. The StreamedContent will be garbage collected normally once the ben keeping a reference to the instance is moved out of context. There is no clear need to close the input stream.

---
+++ src/main/java/entrypoint/jsf/SessionScopedBean.java
@@ -0,0 +1,60 @@
+package entrypoint.jsf;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author b7godin
+ */
+@Named("sessionScopedBean")
+@SessionScoped
+public class SessionScopedBean implements Serializable {
+
+    private static String PROGRAMTIC_RESOURCE_PATH_TO_LABRADOR_IMG = "resources/img/blackLabrador.jpg";
+    private static String PROGRAMTIC_PATH_TO_ASK_FOR_MIME_TYPE = "img/blackLabrador.jpg";
+
+    @Inject
+    private transient FacesContext facesContext;
+
+    private transient StreamedContent streamedContent;
+
+   
+
+    @PostConstruct
+    private void postConstruct() {
+        InputStream inputStream = facesContext.getExternalContext().getResourceAsStream(
+                PROGRAMTIC_RESOURCE_PATH_TO_LABRADOR_IMG);
+        String mimeType = facesContext.getExternalContext().getMimeType(PROGRAMTIC_PATH_TO_ASK_FOR_MIME_TYPE);
+        streamedContent = new DefaultStreamedContent(inputStream, mimeType);
+    }
+
+    public String getPing() {
+        return "ping";
+    }
+    public String getImageResource() {
+        return "img/blackLabrador.jpg";
+    }
+
+    /**
+     * Get Streamed content
+     */
+    public StreamedContent getStreamedContent() {
+        return streamedContent;
+    }
+
+
+}
